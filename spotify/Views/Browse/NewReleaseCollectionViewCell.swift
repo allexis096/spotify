283b01c1fb68f734c0ea02436676dd155182ac2c
allexis <allexissoares@gmail.com>
2022-11-15 22:17:04 -0300

    feat: add collection view with delegate and datasource methods

---
+++ spotify/Views/Browse/NewReleaseCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  NewReleaseCollectionViewCell.swift
+//  spotify
+//
+//  Created by allexis figueiredo on 13/11/22.
+//
+
+import UIKit
+
+class NewReleaseCollectionViewCell: UICollectionViewCell {
+    static let identifier = "NewReleaseCollectionViewCell"
+}
---
+++ spotify/Views/Browse/RecommendedTrackCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RecommendedTrackCollectionViewCell.swift
+//  spotify
+//
+//  Created by allexis figueiredo on 13/11/22.
+//
+
+import UIKit
+
+class RecommendedTrackCollectionViewCell: UICollectionViewCell {
+    static let identifier = "RecommendedTrackCollectionViewCell"
+}
