3b7a3e092005bd2f998a1680464d7b8506e284c0
allexis <allexissoares@gmail.com>
2022-12-24 12:54:47 -0300

    feat: add playlistView header and connect vc to api

---
+++ spotify/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  spotify
+//
+//  Created by allexis figueiredo on 24/12/22.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
